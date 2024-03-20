62d6d211f8abbddb4b99e385711a21bc34a4e681
code-syl <ejjvandelaar@gmail.com>
2024-03-20 10:48:35 +0100

    added ping endpoint

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const ADDRESS string = "0.0.0.0"
+const PORT string = "8080"
+
+type response struct {
+	Message    string `json:"message"`
+	HttpStatus int    `json:"http_status"`
+	Success    bool   `json:"success"`
+}
+
+var pong = response{
+	Message:    "pong!",
+	HttpStatus: 200,
+	Success:    true,
+}
+
+func ping(c *gin.Context) {
+	c.IndentedJSON(pong.HttpStatus, pong)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fullAddress := strings.Join([]string{ADDRESS, PORT}, ":")
+	fmt.Print("Starting server on")
+	fmt.Println(fullAddress)
+
+	router := gin.Default()
+	router.GET("/ping", ping)
+	router.Run(fullAddress)
 }
